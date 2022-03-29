c2a04f20542b34b9da1840f2357610e7f7bf354d
stooooopu <jyg5193@naver.com>
2022-03-29 09:21:47 +0900

    fix 내용 추가

--- 0328_숙제/algorithm/index.js
+++ 0328_숙제/algorithm/index.js
@@ -3,5 +3,17 @@
 let array = [1, 10, 4, 3, 5];
 let count = 0; //총 몇회전 했는지 알아내는 변수
 
+var temp = 0;
+for (var i=1; i<array.length; i++){
+    for(var j=0; j<array.length; j++){
+        if(array[i]<array[j]){
+            temp=array[i];
+            array[i]=array[j];
+            array[j]=temp;
+            ++count;
+        }
+    }
+}
+
 console.log(array);
 console.log(count);
